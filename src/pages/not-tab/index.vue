810ade67e765dc7d69d1f28d2eaa370747fa3ab5
lin_yu <lin_yu@hunliji.com>
2019-03-20 21:59:33 +0800

    feat: change

--- src/pages/not-tab/index.vue
+++ src/pages/not-tab/index.vue
@@ -1,12 +1,67 @@
 <template>
     <div class="not-tab">
-      <img class="svg" src="https://s.haicaoyun.com/mp/svg/baseline-3d_rotation-24px.svg" alt="">
+      <input type="digit" :value="info.name" @blur="handleNameBlur">
+      <input type="number" :value="info.phone" @blur="handlePhoneBlur">
+      <input type="text" :value="info.weChat" @blur="handleWeChatBlur">
+      <picker mode="region" @change="handleRegionChange" :value="info.region">
+        <input type="text" :value="totalRegion" disabled>
+      </picker>
+      <input class="edit" placeholder-class="placeholder"
+             type="digit"
+             maxlength="10"
+             placeholder="请输入价格"
+             :value="info.price"
+             @blur="handlePriceChange" />
     </div>
 </template>
 
 <script>
   export default {
-    name: 'index'
+    name: 'index',
+    data () {
+      return {
+        info: {
+          region: ['', '', ''],
+          name: 0,
+          phone: 123,
+          weChat: '',
+          price: 1
+        },
+        region: ['', '', ''],
+        name: ''
+      }
+    },
+    computed: {
+      totalRegion () {
+        return `${this.info.region[0]}${this.info.region[1]}`
+      }
+    },
+    methods: {
+      handleNameBlur (e) {
+        const {value: name} = e.target
+        this.info = {
+          ...this.info,
+          name
+        }
+      },
+      handlePhoneBlur (e) {
+        const {value} = e.target
+        this.info.phone = value
+      },
+      handleWeChatBlur (e) {
+        const {value} = e.target
+        this.info.weChat = value
+      },
+      handleRegionChange (e) {
+        const {value} = e.target
+        console.log(value)
+        this.info.region = value
+      },
+      handlePriceChange (e) {
+        const {value: price} = e.target
+        this.info.price = price
+      }
+    }
   }
 </script>
 
@@ -14,8 +69,12 @@
   .not-tab {
     width: 100%;
   }
-  .svg {
-    width: 200px;
-    height: 200px;
+  input {
+    border: 1px solid #000;
+    color: #000;
+    font-size: 24px;
+    line-height: 50px;
+    width: 100%;
+    height: 50px;
   }
 </style>
